22dc3447777934db6dc614a327b8d6c674e50dde
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-07-24 22:38:34 +0300

    Add product search by name or description using Ransack

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,8 @@ class ProductsController < ApplicationController
 
   # GET /products or /products.json
   def index
-    @products = Product.all
+    @q = Product.ransack(params[:q])
+    @products = @q.result.order(created_at: :desc)
   end
 
   # GET /products/1 or /products/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "description", "price", "quantity"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
