c759a5d20af097d6caa04b73aaa804cbb388581f
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-07-24 21:07:23 +0300

    Add validations for Product model to prevent negative price and quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
