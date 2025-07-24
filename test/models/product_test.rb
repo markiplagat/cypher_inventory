31de68a7cc43528385722058b37cbe4472da9fc2
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-07-24 21:08:07 +0300

    Add product tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    product = Product.new(
+      name: "Milk",
+      description: "Fresh milk",
+      price: 2.99,
+      quantity: 15
+    )
+    assert product.valid?
+  end
+
+  test "is invalid without a name" do
+    product = Product.new(name: nil)
+    assert_not product.valid?
+  end
+
+  test "is invalid with negative quantity" do
+    product = Product.new(quantity: -5)
+    assert_not product.valid?
+  end
+
+  test "is invalid with negative price" do
+    product = Product.new(price: -1)
+    assert_not product.valid?
+  end
 end
