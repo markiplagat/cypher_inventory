4eaff1e6f4aa2f2443b0cde11d7c3203bc3a2035
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-07-24 23:30:21 +0300

    Redirect admins to /admin and users to /products after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      rails_admin_path # sends admin users to /admin
+    else
+      products_path    # sends regular users to /products
+    end
+  end
+
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
 end
