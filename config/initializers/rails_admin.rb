20554c3f238d536e2d88d801cd418d525d41959a
markiplagat <markkiplagatkipruttoh@gmail.com>
2025-07-24 23:18:04 +0300

    Protect RailsAdmin dashboard with admin-only access

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -24,6 +24,12 @@ RailsAdmin.config do |config|
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
 
+  config.authenticate_with do
+    redirect_to main_app.root_path unless current_user&.admin?
+  end
+
+  config.current_user_method(&:current_user)
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
